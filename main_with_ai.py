3c9a44083fcc30e655abad05786c3c13b6cdf5d1
prathyusha1993 <prathyusha9310@gmail.com>
2025-03-19 13:52:38 -0700

    day -84 python challenges and practising tic tac toe game

--- main_with_ai.py
+++ main_with_ai.py
@@ -1,3 +1,4 @@
+import random
 
 board = []
 for _ in range(3):
@@ -6,7 +7,6 @@ def draw_board():
     for row in board:
         print(' | '.join(row))
         print('-'*9)
-
 
 def check_winner(player):
     for i in range(3):
@@ -25,23 +25,42 @@ def check_winner(player):
 def is_draw():
     return all(board[i][j] != ' ' for i in range(3) for j in range(3))
 
+def ai_move():
+    empty_cells = [(i, j) for i in range(3) for j in range(3) if board[i][j] == ' ']
+    return random.choice(empty_cells)
+
+
 is_game_over = False
-current_player = 'X'
+player = 'X'
+ai = 'O'
 while not is_game_over:
     draw_board()
-    row, col = map(int, input(f'Player {current_player} provide your input as row and columns(1-3) (eg: 1,2):').split())
-    if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
-        board[row][col] = current_player
 
-        if check_winner(current_player):
+    if player == 'X':
+        try:
+            row, col = map(int, input(f'Player {player} provide your input as row and columns(1-3) (eg: 1,2):').split())
+            row -= 1
+            col -= 1
+        except ValueError:
+            print('Invalid input, please enter twonumber between 1 and 3')
+            continue
+    else:
+        row,col = ai_move()
+        print(f'AI plays at ({row+1} {col+1})')
+
+
+    if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+        board[row][col] = player
+
+        if check_winner(player):
             draw_board()
-            print(f'player {current_player} Wins!')
+            print(f'player {player} Wins!')
             is_game_over = True
         elif is_draw():
             draw_board()
             print('Its a draw')
             is_game_over=True
         else:
-            current_player = 'O' if current_player == 'X' else 'X'
+            player = 'O' if player == 'X' else 'X'
     else:
         print('Invalid move! Try again.')
